e9204fa533cbddb7ee61893e751368aeb376153b
camilothorne <camilo.thorne@gmail.com>
2015-11-16 16:18:07 +0100

    first commit

---
+++ phase-transition/logic/parse.py
@@ -0,0 +1,17 @@
+'''
+Created on Nov 16, 2015
+
+@author: camilo
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
